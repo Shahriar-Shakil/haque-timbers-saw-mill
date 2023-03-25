f5dcf2e3fab1a1913a4519a3e6fcfa2b94a7b583
mrshahriar <toshahriarshakil@gmail.com>
2023-03-25 16:10:01 +0600

    add fb messenger

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,12 @@
+import FacebookMessenger from '@/components/FacebookMessenger'
 import '@/styles/globals.css'
+import { Fragment } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <FacebookMessenger />
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
