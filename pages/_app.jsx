050c994d64d7ff22d99a55051e47d8ed1391b999
mrshahriar <toshahriarshakil@gmail.com>
2023-03-28 03:39:12 +0600

    remove scroll paralax

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,5 @@
 import '@/styles/globals.css'
-import { ParallaxProvider } from 'react-scroll-parallax'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ParallaxProvider>
-      <Component {...pageProps} />
-    </ParallaxProvider>
-  )
+  return <Component {...pageProps} />
 }
